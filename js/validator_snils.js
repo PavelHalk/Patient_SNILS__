227755298bcd3d33e3d01a4a22fec140edb4eff7
PavelHalk <87601402+PavelHalk@users.noreply.github.com>
2022-06-14 15:21:10 +0700

    function validateSnils: validate a type (number or string) of data

--- js/validator_snils.js
+++ js/validator_snils.js
@@ -1,4 +1,9 @@
 /*validator_snils.js for tests*/
 function validateSnils(snils, error) {
-
+    var result = false;
+    if (typeof snils === 'number') {
+        snils = snils.toString();
+    } else if (typeof snils !== 'string') {
+        snils = '';
+    }
 }
