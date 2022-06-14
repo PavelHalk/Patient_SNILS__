043d9ddf9aa7e005c9a371cebf07e52d7fb96d27
PavelHalk <87601402+PavelHalk@users.noreply.github.com>
2022-06-14 17:41:33 +0700

    function QUnit.test: generation input data in data_validation_snils.js

--- js/data_validation_snils.js
+++ js/data_validation_snils.js
@@ -1,5 +1,26 @@
 /*data_validation_snils.js*/
 
 QUnit.test('Snils', function (assert) {
+    var data = [].concat(getTests(3), [
+        ['', false, 1],
+        ['dfdfdsfgvhf', false, 2],
+        ['0000000000', false, 3],
+        ['0123456789', false, 3],
+        ['1234567890', false, 3],
+        ['00000000000', true, null],
+        ['01234567890', false, 4],
+        ['12345678901', false, 4],
+        ['000000000000', false, 3],
+        ['012345678901', false, 3],
+        ['123456789012', false, 3],
+// ----
+        ['08765430300', true, null],
+        ['18765430300', false, 4],
+        ['08865430300', false, 4],
+        ['08766430300', false, 4],
+        ['08765440300', false, 4],
+        ['08765430400', false, 4],
+        ['08765430301', false, 4]
+    ]);
 
 });
