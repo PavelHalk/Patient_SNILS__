9f06f6692e465549902583353d68f865aaaa6306
PavelHalk <87601402+PavelHalk@users.noreply.github.com>
2022-06-14 15:52:01 +0700

    function validateSnils: validate a data (use pattern of integer characters for 0 to 9)

--- js/validator_snils.js
+++ js/validator_snils.js
@@ -9,5 +9,8 @@ function validateSnils(snils, error) {
     if (!snils.length) {
         error.code = 1;
         error.message = 'СНИЛС пуст';
+    } else if (/[^0-9]/.test(snils)) {
+        error.code = 2;
+        error.message = 'СНИЛС может состоять только из цифр';
     }
 }
